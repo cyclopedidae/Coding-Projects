9beee1d70e191adeae93eab6e091bb4f837f5fd0
shauna <62158763+cyclopedidae@users.noreply.github.com>
2023-06-27 19:00:42 -0400

    More Formatting Changes and New Feature
    - added final option to edit balance

--- BanKing.py
+++ BanKing.py
@@ -18,7 +18,6 @@ class Account:
 
 account_obj = []
 
-
 for x in range(num_of_accounts): #create the object names
     y = str(x)
     account_obj.append("account_" + y)
@@ -47,7 +46,7 @@ def menu():
     ]
 
     while on:
-        print("══════════════════════════════════")
+        print("\n\n══════════════════════════════════")
         print("||          M  E  N  U          ||")
         print("══════════════════════════════════\n")
 
@@ -56,14 +55,12 @@ def menu():
 
         print("\n\n\n\n\n\n\n\n")
 
-        choice = input("Select an option (1-3): ")
+        choice = input(f"Select an option (1-{len(available_options)}): ")
 
         if choice == "1": #view money
             view_accounts()
-        
         elif choice == "2": #input paycheck
             edit_account()
-
         elif choice == "3": #quit
             on = False #option to close menu
             if on == False:
@@ -74,76 +71,111 @@ def menu():
 
 def back_menu():
     input("Press enter to return to the menu.")
+    menu()
 
 def view_accounts():
-    total_balance = sum(account_balance)
     print("\n\n")
-    print("======= TOTAL MONEY =======")
-    print("Total Money: $" + "{:0,.2f}".format(total_balance))
-    print("===========================\n\n")
 
     for obj in account_obj:
         if(obj.name == "Cash"):
             print("========== CASH ===========")
-            print(f"{obj.name.strip()}: $" + "{:0,.2f}".format((obj.balance)))
-            
+            print(f"{obj.name.strip()}: $" + "{:0,.2f}".format(obj.balance))
         elif(obj.name == "Chequing Account"):
-            print(f"{obj.name.strip()}: $" + "{:0,.2f}".format((obj.balance)))
+            print(f"{obj.name.strip()}: $" + "{:0,.2f}".format(obj.balance))
             print("===========================\n\n")
-            print("========= SAVINGS =========")
-            
+            print("========= SAVINGS =========")   
         else: #ACCOUNTS: investments, rent, food, emergency funds
-            print(f"{obj.name.strip()}: $" + "{:0,.2f}".format((obj.balance)))
+            print(f"{obj.name.strip()}: $" + "{:0,.2f}".format(obj.balance))
     
     print("===========================\n\n")
+    
+    total_balance = sum(account_balance)
+    print("======= TOTAL MONEY =======")
+    print("Total Money: $" + "{:0,.2f}".format(total_balance))
+    print("===========================\n\n")
+    
     back_menu()
 
 def edit_account():
-    print("Which account would you like to edit?")    
-    choice_a = int(input("Type Here: "))
+    on = True
+    print("\nWhich account would you like to edit?\n")
+    
+    for i, obj in enumerate(account_obj):
+        print(f"{i+1}." + obj.name)
 
     available_options = [
         "Deposit to account",
         "Withdraw from account",
-        "Edit account",
+        "Edit account balance",
+        "Return to menu"
     ]
 
+    choice_a = int(input(f"Select an option (1-{len(available_options)}). ")) - 1
+
+    print("=================")
     print("Available Options:")
+    print("=================")
     for i, option in enumerate(available_options):
-        print(f"{i + 1}. {option}")
+        print(f"{i + 1}. {option}\n")
 
-    choice_b = input("Type Here: ")
+    choice_b = input("Choose an option: ")
 
-    if choice_b == "1":
-        current = float(account_obj[choice_a].balance)
+    while on:
+
+        if choice_b == "1":
+            current = float(account_obj[choice_a].balance)
+            
+            print("How much would you like to deposit?")
+            to_deposit = float(input("Enter amount here: "))
+            
+            new_balance = current + to_deposit
+
+            account_obj[choice_a].balance = new_balance
+
+            save()
+
+            return
         
-        print("How much would you like to deposit?")
-        to_deposit = float(input("Enter amount here: "))
+        elif choice_b == "2":
+            current = float(account_obj[choice_a].balance)
+
+            print("How much would you like to withdraw?")
+            to_withdraw = float(input("Enter amount here: "))
+
+            new_balance = current - to_withdraw
+
+            account_obj[choice_a].balance = new_balance
+
+            save()
+
+            return
         
-        new_balance = current + to_deposit
+        elif choice_b == "3":
+            print(f"Current balance: {account_obj[choice_a].balance}")
+            edited_balance = float(input("What would you like to edit this balance to?"))
+            
+            print(f"New balance: ${account_obj[choice_a].balance}")
+            print("Are you sure you want to keep these changes?")
+            choice = input("Y/N\nEnter here: ")
 
-        account_obj[choice_a].balance = new_balance
+            if choice == "Y" or "y":
+                account_obj[choice_a].balance = edited_balance
+            elif choice == "N" or "n":
+                print("Returning to menu ...")
+                back_menu()
+            else:
+                print("Invalid response. Returning to menu...")
+                back_menu()
 
-        save()
-
-        return
-    
-    elif choice_b == "2":
-        current = float(account_obj[choice_a].balance)
-
-        print("How much would you like to withdraw?")
-        to_withdraw = float(input("Enter amount here: "))
-
-        new_balance = current - to_withdraw
-
-        account_obj[choice_a].balance = new_balance
-
-        save()
-
-        return
-    
-    elif choice_b == "3":
-        return
+            return
+        
+        elif choice_b == "4":
+            on = False
+            print("Returning to menu...")
+            back_menu()
+        
+        else: 
+            print("Invalid option.")
 
 def save():
     file = open("chronic_banking.txt", "w")
